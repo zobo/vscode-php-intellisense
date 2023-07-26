fe8085e88dba003c9e5e40461467fa7f551d8991
Damjan Cvetko <damjan.cvetko@monotek.net>
2023-07-26 10:41:54 +0200

    feat: Add support for EvaluatableExpression

---
+++ src/protocol.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode'
+import {
+    ProtocolRequestType,
+    TextDocumentPositionParams,
+    TextDocumentRegistrationOptions,
+    WorkDoneProgressOptions,
+    WorkDoneProgressParams,
+} from 'vscode-languageclient/node'
+
+interface EvaluatableExpression {
+    expression?: string
+    range: vscode.Range
+}
+
+interface EvaluatableExpressionOptions extends WorkDoneProgressOptions {}
+
+interface EvaluatableExpressionParams extends TextDocumentPositionParams, WorkDoneProgressParams {}
+
+interface EvaluatableExpressionRegistrationOptions
+    extends TextDocumentRegistrationOptions,
+        EvaluatableExpressionOptions {}
+
+export namespace EvaluatableExpressionRequest {
+    export const method: 'textDocument/xevaluatableExpression' = 'textDocument/xevaluatableExpression'
+    //const messageDirection: MessageDirection = MessageDirection.clientToServer;
+    export const type = new ProtocolRequestType<
+        EvaluatableExpressionParams,
+        EvaluatableExpression | null,
+        never,
+        void,
+        EvaluatableExpressionRegistrationOptions
+    >(method)
+}
